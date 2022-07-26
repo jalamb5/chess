e6cc0ef18b4a6c4bc997355b3ab0b1131fad522e
Justin Lamb <justinlamb@me.com>
2022-07-26 11:56:41 -0700

    implement chesspiece and pawn

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -1,1 +1,20 @@
 # frozen_string_literal: true
+
+# Hold values common to all chess pieces
+class ChessPiece
+  attr_accessor :location
+
+  def initialize(starting_location)
+    @location = starting_location
+  end
+end
+
+# Pawn specific values
+class Pawn < ChessPiece
+  attr_reader :symbol
+
+  def initialize(starting_location)
+    super(starting_location)
+    @symbol = 'P'
+  end
+end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,3 +1,31 @@
 # frozen_string_literal: true
 
 require_relative '../lib/chess_pieces'
+
+describe ChessPiece do
+  context '#for' do
+    let(:pawn) { ChessPiece.for(Pawn, :a2) }
+
+    it 'creates a pawn' do
+      expect(pawn).to be_an_instance_of Pawn
+    end
+  end
+
+  describe Pawn do
+    let(:pawn) { Pawn.new(:a2) }
+
+    context '#initialize' do
+      it 'creates a pawn' do
+        expect(pawn).to be_an_instance_of Pawn
+      end
+
+      it 'places the pawn on a board space' do
+        expect(pawn.location).to eq :a2
+      end
+
+      it 'displays "P" as symbol' do
+        expect(pawn.symbol).to eq 'P'
+      end
+    end
+  end
+end
