03ad53b7c9c888586e878ddc9746c9e489d5eacb
Justin Lamb <justinlamb@me.com>
2022-07-26 12:24:04 -0700

    add color, prev_loc, en_passant values/tests

--- lib/chess_pieces.rb
+++ lib/chess_pieces.rb
@@ -2,23 +2,27 @@
 
 # Hold values common to all chess pieces
 class ChessPiece
-  attr_accessor :location
+  attr_accessor :location, :previous_location, :color
 
-  def initialize(starting_location)
+  def initialize(starting_location, color)
     @location = starting_location
+    @previous_location = nil
+    @color = color
   end
 
-  def self.for(type, location)
-    const_get("#{type}").new(location)
+  def self.for(type, color, location)
+    const_get(type.to_s).new(location, color)
   end
 end
 
 # Pawn specific values
 class Pawn < ChessPiece
   attr_reader :symbol
+  attr_accessor :en_passant
 
-  def initialize(starting_location)
-    super(starting_location)
+  def initialize(starting_location, color)
+    super(starting_location, color)
     @symbol = 'P'
+    @en_passant = false
   end
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -4,27 +4,35 @@ require_relative '../lib/chess_pieces'
 
 describe ChessPiece do
   context '#for' do
-    let(:pawn) { ChessPiece.for(Pawn, :a2) }
+    let(:pawn) { ChessPiece.for(Pawn, :W, :a2) }
 
     it 'creates a pawn' do
       expect(pawn).to be_an_instance_of Pawn
     end
+
+    it 'places the pawn on a board space' do
+      expect(pawn.location).to eq :a2
+    end
+
+    it 'makes the pawn white' do
+      expect(pawn.color).to eq :W
+    end
+
+    it 'has no previous location' do
+      expect(pawn.previous_location).to be_nil
+    end
   end
 
   describe Pawn do
-    let(:pawn) { Pawn.new(:a2) }
+    let(:pawn) { Pawn.new(:a2, :W) }
 
     context '#initialize' do
-      it 'creates a pawn' do
-        expect(pawn).to be_an_instance_of Pawn
-      end
-
-      it 'places the pawn on a board space' do
-        expect(pawn.location).to eq :a2
-      end
-
       it 'displays "P" as symbol' do
         expect(pawn.symbol).to eq 'P'
+      end
+
+      it 'is not eligible for en passant' do
+        expect(pawn.en_passant).to eq false
       end
     end
   end
