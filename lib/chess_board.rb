fd2a5ef608d12fe1b6b50aa3423b3c034a9a1858
Justin Lamb <justinlamb@me.com>
2022-07-28 12:30:13 -0700

    remove string literal

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Make an 8x8 board to place pieces upon.
 class ChessBoard
   attr_accessor :board
@@ -7,7 +9,7 @@ class ChessBoard
   end
 
   def pretty_print
-    board_string = ''
+    board_string = String.new
     board_length = board.length
 
     board.each do |row|
@@ -25,5 +27,17 @@ class ChessBoard
 
   def build_board
     Array.new(8) { Array.new(8) { '_' } }
+  end
+
+  def build_board_string(board_length)
+    board.each do |row|
+      board_string << "#{board_length} "
+      board_length -= 1
+      row.each do |i|
+        board_string << "|#{i}"
+      end
+      board_string << "|\n"
+    end
+    board_string << '   a b c d e f g h'
   end
 end
