210752612572693f531d628da896d7deaaa002c7
Justin Lamb <justinlamb@me.com>
2022-07-29 11:11:37 -0700

    separate place and remove piece

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -13,8 +13,8 @@ class ChessBoard
   end
 
   def update_board(piece)
-    board[piece.location[0]][piece.location[1]] = piece.symbol
-    board[piece.previous_location[0]][piece.previous_location[1]] = '_' unless piece.previous_location.nil?
+    place_piece(piece)
+    remove_piece(piece) unless piece.previous_location.nil?
   end
 
   private
@@ -35,5 +35,13 @@ class ChessBoard
       board_string << "|\n"
     end
     board_string << '   a b c d e f g h'
+  end
+
+  def place_piece(piece)
+    board[piece.location[0]][piece.location[1]] = piece.symbol
+  end
+
+  def remove_piece(piece)
+    board[piece.previous_location[0]][piece.previous_location[1]] = '_'
   end
 end
