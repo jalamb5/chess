6429a8436c74b020efaa9b92119feafe27c210b6
Justin Lamb <justinlamb@me.com>
2022-08-18 10:15:33 -0700

    replace pawns with rooks

--- spec/piece_capture_spec.rb
+++ spec/piece_capture_spec.rb
@@ -5,13 +5,13 @@ require_relative '../lib/chess_pieces'
 
 describe Capture do
   context '#capture_piece' do
-    let(:capturing_pawn) { Pawn.new([0, 1], :W) }
-    let(:captured_pawn) { Pawn.new([1, 1], :B) }
-    let(:registry) { [capturing_pawn, captured_pawn] }
+    let(:capturing_rook) { Rook.new([0, 1], :W) }
+    let(:captured_rook) { Rook.new([1, 1], :B) }
+    let(:registry) { [capturing_rook, captured_rook] }
 
     it 'marks a piece as captured' do
-      capturing_pawn.move([1, 1], registry)
-      expect(captured_pawn.captured).to eq(true)
+      capturing_rook.move([1, 1], registry)
+      expect(captured_rook.captured).to eq(true)
     end
   end
 end
