2aad5d339d48cbdd809539ed7587a919adf608a5
Justin Lamb <justinlamb@me.com>
2022-08-23 09:53:25 -0700

    extract Pawn specific movement

---
+++ lib/pawn_movement.rb
@@ -0,0 +1,37 @@
+module Pawn_Movement
+  def legal_pawn(new_location, registry)
+    legal_diagonals = diag_moves
+    if legal_diagonals.include?(new_location)
+      return true if location_occupied(new_location, registry) || legal_en_passant(registry)
+    else
+      legal_moves.include?(new_location) unless location_occupied(new_location, registry)
+    end
+  end
+
+  def legal_en_passant(registry)
+    registry.each do |piece|
+      next unless piece.last_mover && piece.instance_of?(Pawn)
+      return piece if piece.en_passant
+    end
+    false
+  end
+
+  def mark_en_passant(registry)
+    registry.each do |piece|
+      piece.en_passant = false if piece.instance_of?(Pawn)
+    end
+    self.en_passant = true if (location[0] - previous_location[0]).abs == 2
+  end
+
+  def capture_en_passant(new_location, registry)
+    piece_to_capture = legal_en_passant(registry)
+    return piece_to_capture if piece_to_capture == false
+
+    if piece_to_capture.color == :B
+      return piece_to_capture if piece_to_capture.location[0] - new_location[0] == 1 && piece_to_capture.location[1] == new_location[1]
+    else
+      return piece_to_capture if piece_to_capture.location[0] + new_location[0] == 1 && piece_to_capture.location[1] == new_location[1]
+    end
+    false
+  end
+end
--- lib/piece_movement.rb
+++ lib/piece_movement.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require_relative './pawn_movement'
+
 # Mixin for chess_pieces to handle updating location and verifying move legality for a given piece
 module Movement
+  include Pawn_Movement
+
   def move(new_location, registry)
     return 'invalid move' unless legal(new_location, registry)
 
@@ -34,39 +38,41 @@ module Movement
     false
   end
 
-  def legal_pawn(new_location, registry)
-    legal_diagonals = diag_moves
-    if legal_diagonals.include?(new_location)
-      return true if location_occupied(new_location, registry) || legal_en_passant(registry)
-    else
-      legal_moves.include?(new_location) unless location_occupied(new_location, registry)
-    end
-  end
+  # Pawn specific functions
 
-  def legal_en_passant(registry)
-    registry.each do |piece|
-      next unless piece.last_mover && piece.instance_of?(Pawn)
-      return piece if piece.en_passant
-    end
-    false
-  end
+  # def legal_pawn(new_location, registry)
+  #   legal_diagonals = diag_moves
+  #   if legal_diagonals.include?(new_location)
+  #     return true if location_occupied(new_location, registry) || legal_en_passant(registry)
+  #   else
+  #     legal_moves.include?(new_location) unless location_occupied(new_location, registry)
+  #   end
+  # end
 
-  def mark_en_passant(registry)
-    registry.each do |piece|
-      piece.en_passant = false if piece.instance_of?(Pawn)
-    end
-    self.en_passant = true if (location[0] - previous_location[0]).abs == 2
-  end
+  # def legal_en_passant(registry)
+  #   registry.each do |piece|
+  #     next unless piece.last_mover && piece.instance_of?(Pawn)
+  #     return piece if piece.en_passant
+  #   end
+  #   false
+  # end
 
-  def capture_en_passant(new_location, registry)
-    piece_to_capture = legal_en_passant(registry)
-    return piece_to_capture if piece_to_capture == false
+  # def mark_en_passant(registry)
+  #   registry.each do |piece|
+  #     piece.en_passant = false if piece.instance_of?(Pawn)
+  #   end
+  #   self.en_passant = true if (location[0] - previous_location[0]).abs == 2
+  # end
 
-    if piece_to_capture.color == :B
-      return piece_to_capture if piece_to_capture.location[0] - new_location[0] == 1 && piece_to_capture.location[1] == new_location[1]
-    else
-      return piece_to_capture if piece_to_capture.location[0] + new_location[0] == 1 && piece_to_capture.location[1] == new_location[1]
-    end
-    false
-  end
+  # def capture_en_passant(new_location, registry)
+  #   piece_to_capture = legal_en_passant(registry)
+  #   return piece_to_capture if piece_to_capture == false
+
+  #   if piece_to_capture.color == :B
+  #     return piece_to_capture if piece_to_capture.location[0] - new_location[0] == 1 && piece_to_capture.location[1] == new_location[1]
+  #   else
+  #     return piece_to_capture if piece_to_capture.location[0] + new_location[0] == 1 && piece_to_capture.location[1] == new_location[1]
+  #   end
+  #   false
+  # end
 end
