22745b1752328fbea003aeb181a2f39861d36291
Justin Lamb <justinlamb@me.com>
2022-08-24 12:29:02 -0700

    test

--- spec/king_movement_spec.rb
+++ spec/king_movement_spec.rb
@@ -8,7 +8,6 @@ describe KingMovement do
   context '#in_check?' do
     let(:black_king) { ChessPiece.for(King, :B, [0, 0]) }
     let(:white_king) { ChessPiece.for(King, :W, [7, 7]) }
-    let(:registry) { [black_king, white_king] }
 
     it 'returns false when the king is not in check' do
       expect(black_king.in_check?).to eql(false)
@@ -17,6 +16,21 @@ describe KingMovement do
     it 'returns true when the king is in check' do
       white_king.check = true
       expect(white_king.in_check?).to eql(true)
+    end
+  end
+
+  context '#mark_checked' do
+    it 'marks a king as checked after another piece moves' do
+      
+    end
+  end
+
+  context '#checked_move' do
+    let(:checked_king) { ChessPiece.for(King, :B, [0, 0]) }
+    let(:checking_rook) { ChessPiece.for(Rook, :W, [0, 1]) }
+
+    xit 'only allows a checked king to move out of check' do
+
     end
   end
 end
