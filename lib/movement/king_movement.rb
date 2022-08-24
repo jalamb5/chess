5bb2e2baf0f67c8535f059d9302fc36c1ad0f6c1
Justin Lamb <justinlamb@me.com>
2022-08-24 11:35:38 -0700

    #in_check?

--- lib/movement/king_movement.rb
+++ lib/movement/king_movement.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# King specific mixin for Movement module
 module KingMovement
-
+  def in_check?
+    check
+  end
 end
--- spec/king_movement_spec.rb
+++ spec/king_movement_spec.rb
@@ -5,13 +5,18 @@ require_relative '../lib/movement/piece_movement'
 require_relative '../lib/chess_pieces'
 
 describe KingMovement do
-  context '#' do
+  context '#in_check?' do
     let(:black_king) { ChessPiece.for(King, :B, [0, 0]) }
     let(:white_king) { ChessPiece.for(King, :W, [7, 7]) }
     let(:registry) { [black_king, white_king] }
 
-    xit 'determines if a king is in check' do
-      
+    it 'returns false when the king is not in check' do
+      expect(black_king.in_check?).to eql(false)
+    end
+
+    it 'returns true when the king is in check' do
+      white_king.check = true
+      expect(white_king.in_check?).to eql(true)
     end
   end
 end
